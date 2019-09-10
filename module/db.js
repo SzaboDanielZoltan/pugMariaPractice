35f41564f95bb52be7fc1484187677f92b803247
Szabo Daniel Zoltan <szabo.daniel.zoltan@gmail.com>
2019-09-10 21:47:33 +0200

    CRUD with mariaDB

--- module/db.js
+++ module/db.js
@@ -35,9 +35,45 @@ module.exports = class DB {
 
   async read() {
     const sql = `
-    SELECT p.id, p.name, p.price, p.stock, m.name AS manufacturer, p.active, p.insdate
+    SELECT p.id, p.name, p.price, p.stock, p.manufacturer, m.name AS company, p.active, p.insdate
       FROM products AS p
-        INNER JOIN manufacturers AS m ON p.maufacturer = m.id;`;
+        INNER JOIN manufacturers AS m ON p.manufacturer = m.id;`;
+    const result = await this.conn.query(sql);
+    console.log(result);
+    return result;
+  }
+
+  async create(product) {
+    const sql = `
+    INSERT INTO products
+      (name, price, manufacturer, stock, active)
+    VALUES
+      ('${product.name}', ${product.price}, ${product.manufacturer}, ${product.stock}, 1);
+    `;
+    const result = await this.conn.query(sql);
+    return result;
+  }
+
+  async update(product, id) {
+    const sql = `
+    UPDATE products
+      SET
+      name='${product.name}',
+      price=${product.price},
+      manufacturer=${product.manufacturer},
+      stock=${product.stock},
+      active=${product.active}
+    WHERE id=${id};
+    `;
+    const result = await this.conn.query(sql);
+    return result;
+  }
+
+  async delete(id) {
+    const sql = `
+    DELETE FROM products
+    WHERE id=${id};
+    `;
     const result = await this.conn.query(sql);
     return result;
   }
--- routes/products.js
+++ routes/products.js
@@ -11,9 +11,34 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+  if (req.params.id === 'newproduct') {
+    res.render('newproduct');
+  }
   const realData = await db.read();
   const productObject = realData.filter(product => product.id == req.params.id)[0];
-  res.render('details', { product: productObject });
+  res.render('details', {
+    product: productObject,
+    id: req.params.id,
+  });
+});
+
+router.post('/', async (req, res) => {
+  const newProduct = req.body;
+  await db.create(newProduct);
+  res.redirect('/products');
+});
+
+router.post('/:id', async (req, res) => {
+  const editedProduct = req.body;
+  const editId = req.params.id;
+  await db.update(editedProduct, parseInt(editId, 10));
+  res.redirect('/products');
+});
+
+router.get('/delete/:id', async (req, res) => {
+  const deleteId = req.params.id;
+  await db.delete(parseInt(deleteId, 10));
+  res.redirect('/products');
 });
 
 module.exports = router;
